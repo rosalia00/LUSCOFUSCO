dda741b346d96dd60d2b7666cd3a23d312faef60
rosaliaperezabalde <Olatzbilbao0>
2023-01-06 18:14:41 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+
+app = Flask(__name__)
+
+
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'amoramor0.'
+app.config['MYSQL_DB'] = 'luscologin'
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        print(username)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return redirect(url_for('index'))
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+@app.route('/index')
+def index():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('index.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+"""
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+"""
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+const {MongoClient} = require("mongodb");
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+path = require('path');
+app.use(express.static(path.join('../app')));
+
+app.get('/gallery', (req, res) => {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://ud:ud@cluster0.szg5vgf.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db('luscofusco');
+        db.collection('artgallery').find({}).toArray(function (findErr, art) {
+            if (findErr) throw findErr;
+            /*
+            for (let i = 0; i < art.length; i++) {
+                console.log("tres");
+                console.log(art[i]);
+            }*/
+            res.render('gallery.ejs', {artList:art} );
+
+            client.close();
+        });
+    });
+
+});
+
+app.use('/gallery', (req, res, next) => {
+    var MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://ud:ud@cluster0.szg5vgf.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db('luscofusco');
+        db.collection('artgallery').find({}).toArray(function (findErr, art) {
+            if (findErr) throw findErr;
+            const filters = req.query;
+            const filteredArt = art.filter(art => {
+                let isValid = true;
+                for (key in filters) {
+                    console.log(key, art[key], filters[key]);
+                    isValid = isValid && art[key] == filters[key];
+                }
+                return isValid;
+
+                res.send('gallery.ejs', {artList:filteredArt});
+            });
+        });
+    });
+});
+
+app.listen(3000,function () {
+    console.log("server is running on port 3000");
+});
